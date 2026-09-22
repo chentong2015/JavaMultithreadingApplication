a5390757a9fe110b511f6bfa34eeb9299379a57a
chent5 <chen2015tong@gmail.com>
2026-09-22 15:09:51 +0200

    update thread local

--- java-thread/src/main/java/thread_local/JavaThreadLocal.java
+++ java-thread/src/main/java/thread_local/JavaThreadLocal.java
@@ -1,15 +1,18 @@
 package thread_local;
 
-// ThreadLocal:
-// 保护线程的局部变量，变量只被某个线程所独享，实现一个线程的本地存储
-// 如果共享数据的代码能够保证在同一个线程中执行，则将共享数据范围约束在同一个线程之内
-//
-// ThreadLocal底层实现原理:
-// 1. 线程Thread对象都有一个ThreadLocalMap对象，存储<ThreadLocalHashCode, Value>的键值对
-// 2. ThreadLocal对象就是当前线程的ThreadLocalMap的入口，它包含唯一HashCode值，用于在K-V中找到本地的线程变量
-public class ThreadLocalDemo {
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-    // 主线程中的线程变量不能被另一个线程共享
+// TODO. ThreadLocal
+// 线程独享的本地存储数据(缓存)，限制数据被并发线程访问/修改
+// - 管理每个线程各自的DB连接
+// - 控制每个线程Transaction/commit数据提交
+public class JavaThreadLocal {
+
+    private final ThreadLocal<Map<String, List<Long>>> techIdCache =
+            ThreadLocal.withInitial(ConcurrentHashMap::new);
+
     public static void main(String[] args) {
         ThreadLocal<String> threadLocal = new ThreadLocal<>();
         threadLocal.set("value");
@@ -17,12 +20,14 @@ public class ThreadLocalDemo {
         ThreadLocal<Integer> threadLocal1 = new ThreadLocal<>();
         threadLocal1.set(100);
 
+        // TODO. 主线程中的线程变量不能被另一个线程共享
         new Thread(() -> {
             // Can't get value from ThreadLocal of Main
             System.out.println("new thread, get string :" + threadLocal.get());
             System.out.println("new thread, get int :" + threadLocal1.get());
         }).start();
 
+        // TODO. 只有当前主线程能访问数据
         System.out.println(threadLocal.get());
         System.out.println(threadLocal1.get());
 
